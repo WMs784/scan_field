3b2ebc06488538b47cbb740a54ab21858a463248
WMs784 <kosei.t12@gmail.com>
2022-03-10 23:46:57 +0900

    changed type int to char or short int

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include "std.h"
-#define rep(i,n) for(int i = 0;i < (int)n;i++)
+#define rep(i,n) for(short int i = 0;i < (short int)n;i++)
 
 using namespace std;
 
@@ -8,17 +8,17 @@ void setup() {
 }
 void loop() {
     float r = 10.0, theta = 30.0;
-    int div = 15;
+    char div = 15;
     Serial.println("start");
     delay(1000);
     rep(i,180/div){
         set_map(r,div*i);
     }
-    int time = millis();
+    short int time = millis();
     Serial.println(time);
     delay(1000);
     scan();
-    int time2 = millis();
+    short int time2 = millis();
     Serial.println(time);
     delay(1000);
 }
--- scan_simulate.cpp
+++ scan_simulate.cpp
@@ -4,22 +4,22 @@
 #include "queue"
 #include "scan_simulate.h"
 
-#define rep(i,n) for(int i = 0;i <(int)n;i++)
+#define rep(i,n) for(short int i = 0;i <(short int)n;i++)
 
 using namespace std;
 
-const int length = 20,width = 20, det_dis = 10;
-int cur_x = length/2, cur_y = width/2;
-int tg_x = 0,tg_y = 0;
-vector<vector<int> > field_map(length,vector<int>(width,1));
-const int dx[4] = {0,1,0,-1}, dy[4] = {1,0,-1,0};
+const short int length = 20,width = 20, det_dis = 10;
+char cur_x = length/2, cur_y = width/2;
+char tg_x = 0,tg_y = 0;
+vector<vector<char> > field_map(length,vector<char>(width,1));
+const short int dx[4] = {0,1,0,-1}, dy[4] = {1,0,-1,0};
 vector<vector<bool> > seen(length,vector<bool>(width,false));
-queue<int> que;
+queue<short int> que;
 
 //map 0:障害物なし,1:未探査,2:障害物あり 
 
 void set_map(float r,float theta){
-    int x = cur_x + r*sin(theta), y = cur_y + r*cos(theta);
+    short int x = cur_x + r*sin(theta), y = cur_y + r*cos(theta);
     // cout << x << ' ' << y << endl;
     rep(i,length){
         rep(j,width){
@@ -30,15 +30,15 @@ void set_map(float r,float theta){
     }
     // field_map[cur_x][cur_y] = 100;
 }
-int bfs(){
+short int bfs(){
     while (!que.empty()){
-        int xy = que.front();
+        short int xy = que.front();
         // cout << xy << ' ';
         que.pop();
-        int x = xy/100,y = xy%100;
+        short int x = xy/100,y = xy%100;
         seen[x][y] = true;
         rep(dir,4){
-            int xd = x + dx[dir], yd = y + dy[dir];
+            short int xd = x + dx[dir], yd = y + dy[dir];
             if(xd < 0 || xd >= length || yd < 0 || yd >= width)continue;
             if(field_map[xd][yd] == 2)continue;
             if(seen[xd][yd])continue;
@@ -52,7 +52,7 @@ int bfs(){
     return 9999;
 }
 
-vector<float> move(int cur,int goal){
+vector<float> move(short int cur,short int goal){
     vector<float> road(2,0.0);
     if(goal == 9999)return road;
     else{
@@ -67,9 +67,9 @@ vector<float> move(int cur,int goal){
     }
 }
 
-int main(){
+void scan(){
     float r = 10.0, theta = 30.0;
-    int div = 15;
+    short int div = 15;
     rep(i,180/div){
         set_map(r,i*div);
     }
@@ -83,7 +83,28 @@ int main(){
         rep(j,width)cout << field_map[i][j] << ' ';
         cout << endl;
     }
-    int cur = cur_x*100+cur_y;
+    short int cur = cur_x*100+cur_y;
     vector<float> trans = move(cur,bfs());
-    rep(i,2)cout << trans[i] << ' ';
+    rep(i,2)Serial.println(trans[i]);
 }
+
+// int main(){
+//     float r = 10.0, theta = 30.0;
+//     int div = 15;
+//     rep(i,180/div){
+//         set_map(r,i*div);
+//     }
+//     que.push(100*cur_x+cur_y);
+//     cout << bfs() << endl;
+//     rep(i,length){
+//         rep(j,width)cout << seen[i][j] << ' ';
+//         cout << endl;
+//     }
+//     rep(i,length){
+//         rep(j,width)cout << field_map[i][j] << ' ';
+//         cout << endl;
+//     }
+//     int cur = cur_x*100+cur_y;
+//     vector<float> trans = move(cur,bfs());
+//     rep(i,2)cout << trans[i] << ' ';
+// }
